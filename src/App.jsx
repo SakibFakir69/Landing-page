38d98791826f35bc919b68d157e14c5c01299ca0
Sakib Fakir <FakirSakib22232@gmail.com>
2025-06-24 03:32:07 +0600

    start project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+
+
+function App() {
+
+
+  return (
+    <div>
+    <h1 className='text-2xl '>Staasdrt</h1>
+     
+    </div>
+  )
+}
+
+export default App
---
+++ src/router/route.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+
+
+
+
+
+
+
+
+export const route = createBrowserRouter([
+
+    {
+        path:'/',
+        element:<App/>
+    }
+
+])
+
+
